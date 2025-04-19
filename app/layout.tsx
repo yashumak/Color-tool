e97ceeed8dec2c010e17b44cd0e928052b9191cc
Yash Umak <www.yashumak@gmail.com>
2025-04-19 11:17:02 +0530

    update 0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Cool Tool',
+  description: 'A cool tool for doing cool things',
+  generator: 'patil.yash',
 }
 
 export default function RootLayout({
