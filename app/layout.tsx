afaae118738fcefbd75ade10d98bb523dda2eb6a
YASH UMAK <patil.rtx@gmail.com>
2025-10-03 21:06:20 +0530

    Add the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: 'Cool Tool',
   description: 'A cool tool for doing cool things',
   generator: 'patil.yash',
+  icons: {
+    icon: 'https://cdn-icons-png.flaticon.com/512/2071/2071665.png',
+  }
 }
 
 export default function RootLayout({
